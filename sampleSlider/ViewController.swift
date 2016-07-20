06254580a8a35ae628bbacdc735ab36c48589e6b
竹田美雪 <takeda.myk@gmail.com>
2016-07-20 16:15:37 +0800

    initial commit

---
+++ sampleSlider/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  sampleSlider
+//
+//  Created by 竹田美雪 on 7/20/16.
+//  Copyright © 2016 竹田美雪. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var mySlider: UISlider!
+    @IBOutlet weak var myLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let labelText = "スライダー"
+        myLabel.text = labelText
+        
+        // スライダーの設定
+        mySlider.minimumValue = 0
+        mySlider.maximumValue = 100
+        mySlider.value = 20
+        
+    }
+
+    @IBAction func changeSlider(sender: UISlider) {
+        print(sender.value)
+        
+        // TODO: valueが50以下のとき、「音が小さいです」50より大きい時「音が大きいです」とデバッグエリアに表示せよ
+        if sender.value <= 50{
+            print("音が小さいです")
+            myLabel.text = "音が小さいです"
+        }else{
+            print("音が大きいです")
+            myLabel.text = "音が大きいです"
+        }
+        
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
