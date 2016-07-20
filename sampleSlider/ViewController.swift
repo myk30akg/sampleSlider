df4c951573ca1832f929f8136b149f2f5d80c4c5
竹田美雪 <takeda.myk@gmail.com>
2016-07-20 17:38:27 +0800

    現在の値をラベルで表示 (スライダーを動かすと値が変化)

--- sampleSlider/ViewController.swift
+++ sampleSlider/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var mySlider: UISlider!
     @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var sliderVolume: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,10 +24,15 @@ class ViewController: UIViewController {
         mySlider.maximumValue = 100
         mySlider.value = 20
         
+        sliderVolume.text = String(Int(mySlider.value))
+        
     }
 
     @IBAction func changeSlider(sender: UISlider) {
         print(sender.value)
+        
+        let level = String(Int(mySlider.value))
+        sliderVolume.text = level
         
         // TODO: valueが50以下のとき、「音が小さいです」50より大きい時「音が大きいです」とデバッグエリアに表示せよ
         if sender.value <= 50{
